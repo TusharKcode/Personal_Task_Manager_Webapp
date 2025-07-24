34afdbe75b5b1f6f6b74a9129f31476048d21cf2
Tushar Kumar <tkgoyal104@gmail.com>
2025-07-24 11:28:26 +0530

    JS File Updated

---
+++ public_folder/script.js
@@ -0,0 +1,24 @@
+const taskForm = document.getElementById('taskForm');
+const taskInput = document.getElementById('taskInput');
+const taskList = document.getElementById('taskList');
+
+//------------------------ ADD TASK TO LIST ----------------------------------
+taskForm.addEventListener('submit', function(e){
+    e.preventDefault;
+
+    const taskText = taskInput.value.trim();
+    if (taskText === '') return;
+
+    const li = document.createElement('li');
+    li.textContent = taskText;
+
+   //------------------ ADD DELETE BUTTON ------------------ 
+   const deleteBtn = document.createElement('button');
+   deleteBtn.textContent = 'Delete';
+   deleteBtn.onclick = () => li.remove();
+
+   li.appendChild(deleteBtn);
+   taskList.appendChild(li);
+
+   taskInput.value = '';
+});
